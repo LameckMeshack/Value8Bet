9c7dbbbf05ea112106ce869427252dad79864bad
LameckMeshack <otienolameck327@gmail.com>
2024-04-11 15:22:24 +0300

    Fix font styles in user component

--- lib/value8_web/live/user_component.ex
+++ lib/value8_web/live/user_component.ex
@@ -51,7 +51,7 @@ def render(assigns) do
                               <img src="https://gravatar.com/avatar/cb3cc75ea248e54d6c8c248dy990452e11?s=400&d=robohash&r=x" class="w-[50px] h-[50px] inline-block shrink-0 rounded-2xl" alt="">
                             </div>
                             <div class="flex flex-col justify-start">
-                              <a href="javascript:void(0)" class="mb-1 font-semibold duration-200 text-lg/normal text-secondary-inverse hover:text-primary">
+                              <a href="javascript:void(0)" class="mb-1 font-normal duration-200 text-lg/normal text-secondary-inverse hover:text-primary">
                            <p>   <%= user.email %></p>
                            <p>   <%= user.first_name %>  <%= user.first_name %>  </p>
                               </a>
@@ -59,7 +59,7 @@ def render(assigns) do
                           </div>
                         </td>
                         <td class="p-3 pr-0 text-start">
-                          <span class="font-semibold text-light-inverse text-md/normal">
+                          <span class="font-normal text-light-inverse text-md/normal">
                            <%= if user.admin do %>
                             Admin
 
@@ -70,16 +70,16 @@ def render(assigns) do
                           </span>
                         </td>
                         <td class="p-3 pr-0 text-start">
-                          <span class="text-center align-baseline inline-flex px-2 py-1 mr-auto items-center font-semibold text-base/none text-success bg-success-light rounded-lg">
+                          <span class="text-center align-baseline inline-flex px-2 py-1 mr-auto items-center font-normal text-base/none text-success bg-success-light rounded-lg">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
                               <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
                             </svg><%= length(user.bets) %></span>
                         </td>
                         <td class="p-3 pr-12 text-start">
-                          <span class="text-center align-baseline inline-flex px-4 py-3 mr-auto items-center font-semibold text-[.95rem] leading-none text-primary bg-primary-light rounded-lg"> 0 </span>
+                          <span class="text-center align-baseline inline-flex px-4 py-3 mr-auto items-center font-normal text-[.95rem] leading-none text-primary bg-primary-light rounded-lg"> 0 </span>
                         </td>
                         <td class="p-3 pr-12 text-start">
-                           <span class="text-center align-baseline inline-flex px-4 py-3 mr-auto items-center font-semibold text-[.95rem] leading-none text-primary bg-primary-light rounded-lg"> 0 </span>
+                           <span class="text-center align-baseline inline-flex px-4 py-3 mr-auto items-center font-normal text-[.95rem] leading-none text-primary bg-primary-light rounded-lg"> 0 </span>
                         </td>
                         <td class="p-3 flex text-start">
                           <button class="ml-auto relative text-secondary-dark bg-light-dark hover:text-primary flex items-center h-[25px] w-[25px] text-base font-medium leading-normal text-center align-middle cursor-pointer rounded-2xl duration-200 ease-in-out shadow-none border-0 justify-center">
