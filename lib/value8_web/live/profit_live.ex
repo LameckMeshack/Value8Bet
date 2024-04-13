21c0567cbf0cfe58ba97a47ba237a879677d9f3b
LameckMeshack <otienolameck327@gmail.com>
2024-04-14 00:58:38 +0300

    Add ApexCharts library and ProfitLive component

---
+++ lib/value8_web/live/profit_live.ex
@@ -0,0 +1,11 @@
+defmodule Value8Web.ProfitLive do
+  use Phoenix.LiveComponent
+
+  @impl Phoenix.LiveView
+  def render(assigns) do
+    ~H"""
+    <p>This is the profit live component</p>
+    """
+  end
+
+end
